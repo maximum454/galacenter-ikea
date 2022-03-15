57b6d924ce605979d60f18e8ac5cec7c5a50e12c
Maxim Karachyov <karachyov.m@gmail.com>
2022-03-15 16:27:51 +0300

    masonry

--- src/js/galacenter-ikea.js
+++ src/js/galacenter-ikea.js
@@ -1,7 +1,18 @@
 $(function (){
-    $('.ideas-ikea__body').masonry({
+    let optionM = {
         itemSelector: '.ideas-ikea__item',
         columnWidth: '.ideas-ikea__body .ideas-ikea__item',
         animate: true,
+        percentPosition: true,
+    }
+    $('.ideas-ikea__body').masonry(optionM);
+
+    $(window).on('load resize',function () {
+        let width = $(document).width();
+        if (width < 767) {
+            $('.ideas-ikea__body').masonry('destroy')
+        }else{
+            $('.ideas-ikea__body').masonry(optionM);
+        }
     });
 })
