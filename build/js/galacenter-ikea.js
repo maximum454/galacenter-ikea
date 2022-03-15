af56e19369dd7375fea03bfd795c46c9299f8d82
Maxim Karachyov <karachyov.m@gmail.com>
2022-03-15 14:31:22 +0300

    ideas-ikea.scss

--- build/js/galacenter-ikea.js
+++ build/js/galacenter-ikea.js
@@ -1,3 +1,7 @@
 $(function (){
-
+    $('.ideas-ikea__body').masonry({
+        itemSelector: '.ideas-ikea__item',
+        columnWidth: '.ideas-ikea__body .ideas-ikea__item',
+        animate: true,
+    });
 })
